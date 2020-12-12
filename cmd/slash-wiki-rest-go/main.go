6b96f96f77a7c30396c969db14d8a6e9ec5ec967
Luna Graysen <75589876+they-them@users.noreply.github.com>
2020-12-12 13:49:44 -0500

    Adopt gorilla/schema

--- cmd/slash-wiki-rest-go/main.go
+++ cmd/slash-wiki-rest-go/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	Utilities "github.com/they-them/slash-wiki-rest-go/cmd/slash-wiki-rest-go/utilities" 
 	Structures "github.com/they-them/slash-wiki-rest-go/cmd/slash-wiki-rest-go/structures" 
+	"github.com/gorilla/schema"
 	"github.com/gorilla/mux"
 	"encoding/json"
 	"time"
@@ -11,13 +11,27 @@ import (
 	"os"
 )
 
+var decoder = schema.NewDecoder()
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		parameters := Utilities.FormValuesToParameters(request)
-		message := Structures.Message{
+		var message Structures.Message
+		err := request.ParseForm()
+		
+		if nil != err {
+			message = Structures.Message{
+				ResponseType: "ephemeral",
+				Text:         "The form could not be parsed.",
+			}
+		}
+
+		var parameters Structures.Parameters
+		_ = decoder.Decode(&parameters, request.PostForm)
+
+		message = Structures.Message{
 			ResponseType: "in_channel",
-			Text:         fmt.Sprintf("Hello %s, the time is %s.", parameters.UserId, time.Now().Format(time.RFC850)),
+			Text:         fmt.Sprintf("Hello %s, the date and time is %s.", parameters.UserId, time.Now().Format(time.RFC850)),
 		}
 
 		writer.Header().Add("Content-Type", "application/json")
--- cmd/slash-wiki-rest-go/structures/structures.go
+++ cmd/slash-wiki-rest-go/structures/structures.go
@@ -3,18 +3,18 @@ package structures
 type Parameters struct {
 	// Payload fields from "Preparing your app to receive Commands" section in documentation:
 	// https://api.slack.com/interactivity/slash-commands#app_command_handling.
-	Token       string `json:"token"`
-	TeamId      string `json:"team_id"`
-	TeamDomain  string `json:"team_domain"`
-	ChannelId   string `json:"channel_id"`
-	ChannelName string `json:"channel_name"`
-	UserId      string `json:"user_id"`
-	UserName    string `json:"user_name"`
-	Command     string `json:"command"`
-	Text        string `json:"text"`
-	ResponseUrl string `json:"response_url"`
-	TriggerId   string `json:"trigger_id"`
-	ApiAppId    string `json:"api_app_id"`
+	Token       string `schema:"token"`
+	TeamId      string `schema:"team_id"`
+	TeamDomain  string `schema:"team_domain"`
+	ChannelId   string `schema:"channel_id"`
+	ChannelName string `schema:"channel_name"`
+	UserId      string `schema:"user_id"`
+	UserName    string `schema:"user_name"`
+	Command     string `schema:"command"`
+	Text        string `schema:"text"`
+	ResponseUrl string `schema:"response_url"`
+	TriggerId   string `schema:"trigger_id"`
+	ApiAppId    string `schema:"api_app_id"`
 }
 
 type Message struct {
